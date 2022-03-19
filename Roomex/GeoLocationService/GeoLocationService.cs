36c54002b40b3e54860e8565ba4da226aa12b002
Carlos Souto <carlos.p.souto@hotmail.com>
2022-03-19 15:46:41 +0000

    Add solution structure

---
+++ Roomex/GeoLocationService/GeoLocationService.cs
@@ -0,0 +1,17 @@
+﻿namespace GeoLocationService
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	public class GeoLocationService
+	{
+		public double GetDistance(double latitude, double longitude)
+		{
+			//System.Device.Location.GeoCoordinate coordinate;
+
+		}
+	}
+}
---
+++ Roomex/WebApi.Host.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace WebApi.Host.IntegrationTests
+{
+	using System;
+	using Xunit;
+
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+
+		}
+	}
+}
